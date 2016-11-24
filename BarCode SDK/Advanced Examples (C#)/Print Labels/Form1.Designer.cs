107f1994caf46168508584c4681a494a744f8ebe
bytescout-es <estarkov@gmail.com>
2016-11-24 15:44:09 +0500

    = regular update

---
+++ BarCode SDK/Advanced Examples (C#)/Print Labels/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace PrintLabels
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.buttonPrint = new System.Windows.Forms.Button();
+			this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+			this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+			this.printDialog1 = new System.Windows.Forms.PrintDialog();
+			this.SuspendLayout();
+			// 
+			// buttonPrint
+			// 
+			this.buttonPrint.Location = new System.Drawing.Point(12, 12);
+			this.buttonPrint.Name = "buttonPrint";
+			this.buttonPrint.Size = new System.Drawing.Size(260, 57);
+			this.buttonPrint.TabIndex = 0;
+			this.buttonPrint.Text = "Draw And Print Cards";
+			this.buttonPrint.UseVisualStyleBackColor = true;
+			this.buttonPrint.Click += new System.EventHandler(this.buttonPrint_Click);
+			// 
+			// printPreviewDialog1
+			// 
+			this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+			this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+			this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+			this.printPreviewDialog1.Document = this.printDocument1;
+			this.printPreviewDialog1.Enabled = true;
+			this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+			this.printPreviewDialog1.Name = "printPreviewDialog1";
+			this.printPreviewDialog1.UseAntiAlias = true;
+			this.printPreviewDialog1.Visible = false;
+			// 
+			// printDocument1
+			// 
+			this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+			// 
+			// printDialog1
+			// 
+			this.printDialog1.Document = this.printDocument1;
+			this.printDialog1.UseEXDialog = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 156);
+			this.Controls.Add(this.buttonPrint);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonPrint;
+		private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+		private System.Drawing.Printing.PrintDocument printDocument1;
+		private System.Windows.Forms.PrintDialog printDialog1;
+	}
+}
+
