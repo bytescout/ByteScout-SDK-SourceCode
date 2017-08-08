632ed7bf48cfb6686ace4f0b09d18091c3bf2562
estar73 <estarkov@gmail.com>
2017-08-08 22:38:16 +0500

    = regular update

---
+++ BarCode SDK/Visual C++/Common Example/BarcodeGenerationExample.cpp
@@ -0,0 +1,45 @@
+#include "stdafx.h"
+
+#import "Bytescout.BarCode.tlb" raw_interfaces_only
+
+using namespace Bytescout_BarCode;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	// Initialize COM.
+	HRESULT hr = CoInitialize(NULL);
+
+	// Create the interface pointer.
+	IBarcodePtr pIBarcode(__uuidof(Barcode));
+
+	// set the registration name and key
+	BSTR regname = ::SysAllocString(L"DEMO");
+	pIBarcode->put_RegistrationName(regname);
+	::SysFreeString(regname);
+
+	BSTR regkey = ::SysAllocString(L"DEMO");
+	pIBarcode->put_RegistrationKey(regkey);
+	::SysFreeString(regkey);
+
+	// Set barcode type (symbology)
+	pIBarcode->put_Symbology(SymbologyType_Code128);
+
+	// Set barcode value
+	BSTR value = ::SysAllocString(L"Abc123");
+	pIBarcode->put_Value(value);
+	::SysFreeString(value);
+
+	// Save barcode image
+	BSTR fileName = ::SysAllocString(L"result.png");
+	pIBarcode->SaveImage(fileName);
+	::SysFreeString(fileName);
+
+	pIBarcode->Release();
+
+	// Uninitialize COM.
+	CoUninitialize();
+
+	return 0;
+}
+
