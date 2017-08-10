ec6968e92133d8f1c83906410c9bf65ccfe9a2bb
estar73 <estarkov@gmail.com>
2017-08-10 08:14:24 +0000

    = regular update

---
+++ _Cross-Product Samples/Generate Barcodes From Spreadsheet (VBScript)/generate_barcodes.vbs
@@ -0,0 +1,35 @@
+'*** Prepare Spreadsheet object
+Set spreadsheet = CreateObject("Bytescout.Spreadsheet.Spreadsheet")
+spreadsheet.RegistrationName = "demo"
+spreadsheet.RegistrationKey = "demo"
+' Load Excel document
+spreadsheet.LoadFromFile("test.xlsx")
+' Take the first worksheet
+Set worksheet = spreadsheet.Worksheet(0)
+
+'*** Prepare Barcode object
+Set barcode = CreateObject("Bytescout.BarCode.Barcode")
+barcode.RegistrationName = "demo"
+barcode.RegistrationKey = "demo"
+' Set barcode type to PDF417
+barcode.Symbology = 13 ' 13 means PDF417 barcode
+' Set higher resoultion (300 is good for printing)
+barcode.ResolutionX = 300
+barcode.ResolutionY = 300
+' Show barcode caption (remove these lines if you don't need it)
+barcode.DrawCaption = True
+barcode.DrawCaptionFor2DBarcodes = True
+
+' Run through worksheet rows
+For i = 0 To worksheet.NotEmptyRowMax - 1
+	' Get barcode value from first worksheet column 
+	barcode.Value = worksheet.Item(i, 0).ValueAsString
+	' Fit barcode into 40x15 mm rectangle
+	barcode.FitInto_3 40, 15, 4 ' 4 means millimeter units
+	' Save barcode image to file
+	barcode.SaveImage("barcode" & i & ".png")
+Next
+
+' Release objects
+Set spreadsheet = Nothing
+Set barcode = Nothing
---
+++ _Cross-Product Samples/Regex Search And Highlight In PDF Document (C#)/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using Bytescout.PDFExtractor;
+using Bytescout.PDF;
+using Pen = Bytescout.PDF.Pen;
+using SolidBrush = Bytescout.PDF.SolidBrush;
+
+namespace SearchAndHighlightExample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const string inputFile = @"sample.pdf";
+			const int pageIndex = 0;
+			const string searchPattern = "\\d+\\.\\d+";
+
+			// Prepare TextExtractor
+			using (TextExtractor textExtractor = new TextExtractor("demo", "demo"))
+			{
+				textExtractor.RegexSearch = true;
+				textExtractor.LoadDocumentFromFile(inputFile);
+
+				// Load document with PDF SDK
+				using (Document pdfDocument = new Document(inputFile))
+				{
+					Page pdfDocumentPage = pdfDocument.Pages[pageIndex];
+					Canvas canvas = pdfDocumentPage.Canvas;
+
+					SolidBrush fillBrush = new SolidBrush(new ColorRGB(255, 0, 0));
+					fillBrush.Opacity = 50; // make the brush transparent
+
+					// Search for pattern and highlight found pieces
+					if (textExtractor.Find(pageIndex, searchPattern, caseSensitive: false))
+					{
+						do
+						{
+							foreach (var foundPiece in textExtractor.FoundText.Elements)
+							{
+								// Inflate the rectangle a bit
+								RectangleF rect = RectangleF.Inflate(foundPiece.Bounds, 1, 2);
+								// Draw rectangle over the PDF page
+								canvas.DrawRectangle(fillBrush, rect);
+							}
+						} while (textExtractor.FindNext());
+					}
+
+					// Save as new PDF document
+					pdfDocument.Save("result.pdf");
+
+					Process.Start("result.pdf");
+				}
+			}
+		}
+	}
+}
