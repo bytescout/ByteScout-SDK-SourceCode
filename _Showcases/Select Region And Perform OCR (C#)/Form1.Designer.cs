f165e21b6d2bdd368b53bc31c1c0061bc6c66b9f
estar73 <estarkov@gmail.com>
2018-08-07 23:02:17 -0700

    = regular update

---
+++ _Showcases/Select Region And Perform OCR (C#)/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace SelectRegionAndPerformOCR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.pdfViewerControl1 = new Bytescout.PDFViewer.PDFViewerControl();
+            this.btnRunOCR = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(971, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(114, 22);
+            this.toolStripButton1.Text = "Load document";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // pdfViewerControl1
+            // 
+            this.pdfViewerControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pdfViewerControl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pdfViewerControl1.Location = new System.Drawing.Point(12, 28);
+            this.pdfViewerControl1.MouseMode = Bytescout.PDFViewer.MouseMode.Selection;
+            this.pdfViewerControl1.Name = "pdfViewerControl1";
+            this.pdfViewerControl1.RegistrationKey = null;
+            this.pdfViewerControl1.RegistrationName = null;
+            this.pdfViewerControl1.ResetRotationOnPageChange = false;
+            this.pdfViewerControl1.Scale = 100;
+            this.pdfViewerControl1.SelectionColor = System.Drawing.Color.Red;
+            this.pdfViewerControl1.ShowImageObjects = true;
+            this.pdfViewerControl1.ShowTextObjects = true;
+            this.pdfViewerControl1.ShowVectorObjects = true;
+            this.pdfViewerControl1.Size = new System.Drawing.Size(766, 490);
+            this.pdfViewerControl1.TabIndex = 1;
+            // 
+            // btnRunOCR
+            // 
+            this.btnRunOCR.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRunOCR.Enabled = false;
+            this.btnRunOCR.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnRunOCR.Location = new System.Drawing.Point(784, 77);
+            this.btnRunOCR.Name = "btnRunOCR";
+            this.btnRunOCR.Size = new System.Drawing.Size(175, 39);
+            this.btnRunOCR.TabIndex = 2;
+            this.btnRunOCR.Text = "Run OCR";
+            this.btnRunOCR.UseVisualStyleBackColor = true;
+            this.btnRunOCR.Click += new System.EventHandler(this.btnRunOCR_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(784, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 46);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Load document, select region and press \"Run OCR\" button";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 530);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRunOCR);
+            this.Controls.Add(this.pdfViewerControl1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private Bytescout.PDFViewer.PDFViewerControl pdfViewerControl1;
+        private System.Windows.Forms.Button btnRunOCR;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ _Showcases/Select Region And Perform OCR (C#)/Program.cs
@@ -0,0 +1,33 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Free Trial Sign Up: https://secure.bytescout.com/users/sign_up //
+//                                                                                           //
+// Copyright © 2017-2018 ByteScout Inc. All rights reserved.                                 //
+// http://www.bytescout.com                                                                  //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SelectRegionAndPerformOCR
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
