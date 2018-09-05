82ff93c161ad8feaf959dfe9dfc0702e3f18d5f2
estar73 <estarkov@gmail.com>
2018-09-05 10:52:37 -0700

    = regular update

---
+++ Invoice Parser SDK/C#/Parse Folder To CSV/Program.cs
@@ -0,0 +1,77 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Free Trial Sign Up: https://secure.bytescout.com/users/sign_up //
+//                                                                                           //
+// Copyright © 2017-2018 ByteScout Inc. All rights reserved.                                 //
+// http://www.bytescout.com                                                                  //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using System.Diagnostics;
+using System.Text;
+using ByteScout.InvoiceParser;
+
+namespace ParseFolderToCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFolder = @".\invoices";
+            string outputFile = @".\result.csv";
+
+            // Create InvoiceParser instance
+            using (InvoiceParser parser = new InvoiceParser("demo", "demo"))
+            {
+                // Disable fallback to default generic template if document is nor recognized
+                parser.FallbackToGenericTemplate = false;
+
+                // Setup CSV output
+                CSVOptions csvOptions = new CSVOptions
+                {
+                    Encoding = Encoding.UTF8,
+                    SeparatorCharacter = ",",
+                    QuotationCharacter = "\"",
+                    UnwrapMultilineValues = true
+                };
+
+                // Parse all document in input folder and save results to CSV file
+                BatchProcessing.ParseFolderToCSV(parser, inputFolder, outputFile, csvOptions, ProcessingCallback);
+            }
+
+            // Open generated CSV file in default associated application (for demo purpose)
+            Process.Start(outputFile);
+        }
+
+        /// <summary>
+        /// Callback method to display progress and interrupt processing if needed. 
+        /// </summary>
+        /// <param name="fileName">File name.</param>
+        /// <param name="parsingResult">Parsing result.</param>
+        /// <param name="progress">Current progress in percents.</param>
+        /// <param name="innerException">Exception information in case of error.</param>
+        /// <param name="cancel">Set to <c>true</c> to interrupt the batch processing.</param>
+        static void ProcessingCallback(string fileName, bool parsingResult, double progress, Exception innerException, ref bool cancel)
+        {
+            Console.WriteLine($"{progress}% Processed file \"{fileName}\"");
+            Console.WriteLine($"  Result: {parsingResult}");
+            if (!parsingResult)
+            {
+                if (innerException != null)
+                    Console.WriteLine(innerException.ToString());
+
+                Console.Write("Continue processing? (y/n): ");
+                int key = Console.Read();
+                if (key == 'n')
+                {
+                    Console.WriteLine("Interrupted.");
+                    cancel = true;
+                }
+            }
+        }
+    }
+}
---
+++ Invoice Parser SDK/VB.NET/Parse Folder To CSV/Module1.vb
@@ -0,0 +1,68 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Free Trial Sign Up: https://secure.bytescout.com/users/sign_up '
+'                                                                                           '
+' Copyright © 2017-2018 ByteScout Inc. All rights reserved.                                 '
+' http://www.bytescout.com                                                                  '
+'                                                                                           '
+'*******************************************************************************************'
+
+
+Imports System.Text
+Imports ByteScout.InvoiceParser
+
+Module Module1
+
+    Sub Main()
+
+        Dim inputFolder As String = ".\invoices"
+        Dim outputFile As String = ".\result.csv"
+
+        ' Create InvoiceParser instance
+        Using parser As InvoiceParser = New InvoiceParser("demo", "demo")
+
+            ' Disable fallback to default generic template if document is nor recognized
+            parser.FallbackToGenericTemplate = False
+
+            ' Setup CSV output
+            Dim csvOptions As CSVOptions = New CSVOptions()
+            csvOptions.Encoding = Encoding.UTF8
+            csvOptions.SeparatorCharacter = ","
+            csvOptions.QuotationCharacter = """"
+            csvOptions.UnwrapMultilineValues = True
+
+            ' Parse all document in input folder and save results to CSV file
+            BatchProcessing.ParseFolderToCSV(parser, inputFolder, outputFile, csvOptions, AddressOf ProcessingCallback)
+
+        End Using
+
+        ' Open generated CSV file in default associated application (for demo purpose)
+        Process.Start(outputFile)
+
+    End Sub
+
+    Sub ProcessingCallback(fileName As String, parsingResult As Boolean, progress As Double, innerException As Exception, ByRef cancel As Boolean)
+
+        Console.WriteLine($"{progress}% Processed file ""{fileName}""")
+        Console.WriteLine($"  Result: {parsingResult}")
+
+        If Not parsingResult 
+
+            If innerException IsNot Nothing
+                Console.WriteLine(innerException.ToString())
+            End If
+
+            Console.Write("Continue processing? (y/n): ")
+            Dim key As Integer = Console.Read()
+            If key = 110 ' n
+                Console.WriteLine("Interrupted.")
+                cancel = True
+            End If
+
+        End If
+
+    End Sub
+
+End Module
