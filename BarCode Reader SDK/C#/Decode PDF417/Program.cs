6dad8e98e9da8e7da4665afff02540afb8982b90
estar73 <estarkov@gmail.com>
2019-01-28 06:56:27 +0000

    = regular update

--- BarCode Reader SDK/C#/Decode PDF417/Program.cs
+++ BarCode Reader SDK/C#/Decode PDF417/Program.cs
@@ -12,7 +12,6 @@
 
 using System;
 using System.IO;
-
 using Bytescout.BarCodeReader;
 
 namespace ReadPDF417
@@ -39,6 +38,9 @@ namespace ReadPDF417
             {
                 Console.WriteLine("Found barcode with type '{0}' and value '{1}'", barcode.Type, barcode.Value);
             }
+			
+			// Cleanup
+			reader.Dispose();
 
             Console.WriteLine("Press any key to exit..");
             Console.ReadKey();
