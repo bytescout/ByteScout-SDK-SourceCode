1f8d5b963e44fb7e28c63c791193a496ea602419
estar73 <estarkov@gmail.com>
2019-01-28 06:34:56 +0000

    = regular update

--- BarCode Reader SDK/C#/Decode Patch Code/Program.cs
+++ BarCode Reader SDK/C#/Decode Patch Code/Program.cs
@@ -12,7 +12,6 @@
 
 using System;
 using System.IO;
-
 using Bytescout.BarCodeReader;
 
 namespace ReadPatchCode
@@ -41,6 +40,9 @@ namespace ReadPatchCode
             {
                 Console.WriteLine("Found barcode with type '{0}' and value '{1}'", barcode.Type, barcode.Value);
             }
+			
+			// Cleanup
+			reader.Dispose();
 
             Console.WriteLine("Press any key to exit..");
             Console.ReadKey();
