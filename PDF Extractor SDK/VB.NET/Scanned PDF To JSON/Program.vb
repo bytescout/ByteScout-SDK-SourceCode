95f7d4972f4a7d64dcdb57cda9a3269bda34cf4c
estar73 <estarkov@gmail.com>
2019-01-31 15:17:00 +0000

    = regular update

---
+++ PDF Extractor SDK/VB.NET/Scanned PDF To JSON/Program.vb
@@ -0,0 +1,87 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Free Trial Sign Up: https://secure.bytescout.com/users/sign_up '
+'                                                                                           '
+' Copyright © 2017-2018 ByteScout Inc. All rights reserved.                                 '
+' http://www.bytescout.com                                                                  '
+'                                                                                           '
+'*******************************************************************************************'
+
+
+Imports Bytescout.PDFExtractor
+
+' This example demonstrates the use of Optical Character Recognition (OCR) to extract text into json
+' from scanned PDF documents and raster images.
+
+' To make OCR work you should add the following references to your project:
+' "Bytescout.PDFExtractor.dll", "Bytescout.PDFExtractor.OCRExtension.dll".
+
+Class Program
+
+    Friend Shared Sub Main(args As String())
+
+        ' Create Bytescout.PDFExtractor.JSONExtractor instance
+        Dim extractor As New JSONExtractor()
+        extractor.RegistrationName = "demo"
+        extractor.RegistrationKey = "demo"
+
+        ' Load sample PDF document
+        extractor.LoadDocumentFromFile("sample_ocr.pdf")
+
+        ' Enable Optical Character Recognition (OCR)
+        ' in .Auto mode (SDK automatically checks if needs to use OCR or not)
+        extractor.OCRMode = OCRMode.Auto
+
+        ' Set the location of "tessdata" folder containing language data files
+        extractor.OCRLanguageDataFolder = "c:\Program Files\Bytescout PDF Extractor SDK\Redistributable\net2.00\tessdata"
+        
+        ' Set OCR language
+        extractor.OCRLanguage = "eng"  ' "eng" for english, "deu" for German, "fra" for French, "spa" for Spanish etc - according to files in /tessdata
+        ' Find more language files at https://github.com/tesseract-ocr/tessdata/tree/3.04.00
+        
+        ' Set PDF document rendering resolution
+        extractor.OCRResolution = 300
+
+
+        ' You can also apply various preprocessing filters
+        ' to improve the recognition on low-quality scans.
+
+        ' Automatically deskew skewed scans
+        'extractor.OCRImagePreprocessingFilters.AddDeskew()
+
+        ' Remove vertical or horizontal lines (sometimes helps to avoid OCR engine's page segmentation errors)
+        'extractor.OCRImagePreprocessingFilters.AddVerticalLinesRemover()
+        'extractor.OCRImagePreprocessingFilters.AddHorizontalLinesRemover()
+
+        ' Repair broken letters
+        'extractor.OCRImagePreprocessingFilters.AddDilate()
+
+        ' Remove noise
+        'extractor.OCRImagePreprocessingFilters.AddMedian()
+
+        ' Apply Gamma Correction
+        'extractor.OCRImagePreprocessingFilters.AddGammaCorrection()
+
+        ' Add Contrast
+        'extractor.OCRImagePreprocessingFilters.AddContrast(20)
+
+
+        ' (!) You can use new OCRAnalyzer class to find an optimal set of image preprocessing 
+        ' filters for your specific document.
+        ' See "OCR Analyser" example.
+
+
+        ' Save extracted text to file
+        extractor.SaveJSONToFile("output.json")
+
+        ' Cleanup
+        extractor.Dispose()
+
+        ' Open output file in default associated application
+        System.Diagnostics.Process.Start("output.json")
+
+    End Sub
+    
+End Class
