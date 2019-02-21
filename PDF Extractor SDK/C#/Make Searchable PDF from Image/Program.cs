06bad7c04b35a183f762572b5c908ddcbcfa68b7
estar73 <estarkov@gmail.com>
2019-02-21 19:21:58 +0000

    = regular update

---
+++ PDF Extractor SDK/C#/Make Searchable PDF from Image/Program.cs
@@ -0,0 +1,53 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Free Trial Sign Up: https://secure.bytescout.com/users/sign_up //
+//                                                                                           //
+// Copyright © 2017-2018 ByteScout Inc. All rights reserved.                                 //
+// http://www.bytescout.com                                                                  //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System.Diagnostics;
+using Bytescout.PDFExtractor;
+
+// To make OCR work you should add to your project references to Bytescout.PDFExtractor.dll and Bytescout.PDFExtractor.OCRExtension.dll 
+
+namespace MakeSearchablePDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Bytescout.PDFExtractor.TextExtractor instance
+            SearchablePDFMaker searchablePDFMaker = new SearchablePDFMaker();
+            searchablePDFMaker.RegistrationName = "demo";
+            searchablePDFMaker.RegistrationKey = "demo";
+
+            // Load sample Image document
+            searchablePDFMaker.LoadDocumentFromFile("sample_ocr.jpg");
+            
+            // Set the location of "tessdata" folder containing language data files
+            searchablePDFMaker.OCRLanguageDataFolder = @"c:\Program Files\Bytescout PDF Extractor SDK\Redistributable\net2.00\tessdata\";
+
+            // Set OCR language
+            searchablePDFMaker.OCRLanguage = "eng"; // "eng" for english, "deu" for German, "fra" for French, "spa" for Spanish etc - according to files in /tessdata
+
+            // Set PDF document rendering resolution
+            searchablePDFMaker.OCRResolution = 300;
+
+            // Save extracted text to file
+            searchablePDFMaker.MakePDFSearchable("output.pdf");
+
+            // Cleanup
+            searchablePDFMaker.Dispose();
+
+            // Open output file in default associated application
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("output.pdf");
+            processStartInfo.UseShellExecute = true;
+            Process.Start(processStartInfo);
+        }
+    }
+}
---
+++ PDF Extractor SDK/VB.NET/Make Searchable PDF from Image/Module1.vb
@@ -0,0 +1,49 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Free Trial Sign Up: https://secure.bytescout.com/users/sign_up '
+'                                                                                           '
+' Copyright © 2017-2018 ByteScout Inc. All rights reserved.                                 '
+' http://www.bytescout.com                                                                  '
+'                                                                                           '
+'*******************************************************************************************'
+
+
+Imports Bytescout.PDFExtractor
+
+Module Module1
+
+    Sub Main()
+
+        ' Create Bytescout.PDFExtractor.TextExtractor instance
+        Dim searchablePdfMaker As SearchablePDFMaker = New SearchablePDFMaker()
+        searchablePdfMaker.RegistrationName = "demo"
+        searchablePdfMaker.RegistrationKey = "demo"
+
+        ' Load sample Image document
+        searchablePdfMaker.LoadDocumentFromFile("sample_ocr.jpg")
+
+        ' Set the location of "tessdata" folder containing language data files
+        searchablePdfMaker.OCRLanguageDataFolder = "c:\Program Files\Bytescout PDF Extractor SDK\Redistributable\net2.00\tessdata\"
+
+        ' Set OCR language
+        searchablePdfMaker.OCRLanguage = "eng" ' "eng" for english, "deu" for German, "fra" for French, "spa" for Spanish etc - according to files in /tessdata
+
+        ' Set PDF document rendering resolution
+        searchablePdfMaker.OCRResolution = 300
+
+        ' Save extracted text to file
+        searchablePdfMaker.MakePDFSearchable("output.pdf")
+
+        ' Cleanup
+        searchablePDFMaker.Dispose()
+
+        ' Open output file in default associated application
+        Dim processStartInfo As New ProcessStartInfo("output.pdf")
+        processStartInfo.UseShellExecute = True
+        System.Diagnostics.Process.Start(processStartInfo)
+
+    End Sub
+
+End Module
