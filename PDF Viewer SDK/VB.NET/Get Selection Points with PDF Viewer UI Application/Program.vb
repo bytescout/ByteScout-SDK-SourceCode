bb7a380e33bfa01efafeaa3efc120ef54603ee78
estar73 <estarkov@gmail.com>
2019-05-06 11:25:47 +0000

    = regular update

---
+++ PDF Viewer SDK/VB.NET/Get Selection Points with PDF Viewer UI Application/Program.vb
@@ -0,0 +1,28 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Free Trial Sign Up: https://secure.bytescout.com/users/sign_up '
+'                                                                                           '
+' Copyright © 2017-2018 ByteScout Inc. All rights reserved.                                 '
+' http://www.bytescout.com                                                                  '
+'                                                                                           '
+'*******************************************************************************************'
+
+
+Imports System.Collections.Generic
+Imports System.Windows.Forms
+
+NotInheritable Class Program
+	Private Sub New()
+	End Sub
+	''' <summary>
+	''' The main entry point for the application.
+	''' </summary>
+	<STAThread> _
+	Friend Shared Sub Main()
+		Application.EnableVisualStyles()
+		Application.SetCompatibleTextRenderingDefault(False)
+		Application.Run(New Form1())
+	End Sub
+End Class
