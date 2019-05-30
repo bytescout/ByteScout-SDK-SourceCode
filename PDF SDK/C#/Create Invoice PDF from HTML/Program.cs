7bbe40ce32e266c8e81b4e541a957f556c476729
estar73 <estarkov@gmail.com>
2019-05-30 17:45:38 +0000

    = regular update

---
+++ PDF SDK/C#/Create Invoice PDF from HTML/Program.cs
@@ -0,0 +1,43 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using System.Diagnostics;
+using System.Drawing.Printing;
+using Bytescout.PDF.Converters;
+
+namespace Tables
+{
+    /// <summary>
+    /// This example demonstrates how to create invoice from HTML.
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            using (HtmlToPdfConverter converter = new HtmlToPdfConverter())
+            {
+                converter.PageSize = PaperKind.Letter;
+                converter.Orientation = PaperOrientation.Portrait;
+
+                // Convert input HTML to output pdf
+                converter.ConvertHtmlToPdf("invoice.html", "result.pdf");
+            }
+
+            // Open result document in default associated application (for demo purpose)
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("result.pdf");
+            processStartInfo.UseShellExecute = true;
+            Process.Start(processStartInfo);
+        }
+    }
+}
