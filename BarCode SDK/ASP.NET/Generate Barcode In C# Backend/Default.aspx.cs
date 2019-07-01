e58dd43e7b1db71928b32fc865780e653c5bc09d
estar73 <estarkov@gmail.com>
2019-07-01 12:44:25 +0000

    = regular update

--- BarCode SDK/ASP.NET/Generate Barcode In C# Backend/Default.aspx.cs
+++ BarCode SDK/ASP.NET/Generate Barcode In C# Backend/Default.aspx.cs
@@ -26,6 +26,26 @@ using Bytescout.BarCode;
 
 public partial class _Default : System.Web.UI.Page
 {
+
+    /*
+    IF YOU SEE TEMPORARY FOLDER ACCESS ERRORS: 
+
+    Temporary folder access is required for web application when you use ByteScout SDK in it.
+    If you are getting errors related to the access to temporary folder like "Access to the path 'C:\Windows\TEMP\... is denied" then you need to add permission for this temporary folder to make ByteScout SDK working on that machine and IIS configuration because ByteScout SDK requires access to temp folder to cache some of its data for more efficient work.
+
+    SOLUTION:
+
+    If your IIS Application Pool has "Load User Profile" option enabled the IIS provides access to user's temp folder. Check user's temporary folder
+
+    If you are running Web Application under an impersonated account or IIS_IUSRS group, IIS may redirect all requests into separate temp folder like "c:\temp\".
+
+    In this case
+    - check the User or User Group your web application is running under
+    - then add permissions for this User or User Group to read and write into that temp folder (c:\temp or c:\windows\temp\ folder)
+    - restart your web application and try again
+
+    */
+
     protected void Page_Load(object sender, EventArgs e)
     {
         // Create new barcode
--- BarCode SDK/ASP.NET/Generate Barcode in WebImage Control in VB.NET/Default.aspx.vb
+++ BarCode SDK/ASP.NET/Generate Barcode in WebImage Control in VB.NET/Default.aspx.vb
@@ -13,6 +13,22 @@
 Partial Public Class _Default
     Inherits System.Web.UI.Page
 
+    ' IF YOU SEE TEMPORARY FOLDER ACCESS ERRORS: 
+
+    ' Temporary folder access is required for web application when you use ByteScout SDK in it.
+    ' If you are getting errors related to the access to temporary folder like "Access to the path 'C:\Windows\TEMP\... is denied" then you need to add permission for this temporary folder to make ByteScout SDK working on that machine and IIS configuration because ByteScout SDK requires access to temp folder to cache some of its data for more efficient work.
+
+    ' SOLUTION:
+
+    ' If your IIS Application Pool has "Load User Profile" option enabled the IIS provides access to user's temp folder. Check user's temporary folder
+
+    ' If you are running Web Application under an impersonated account or IIS_IUSRS group, IIS may redirect all requests into separate temp folder like "c:\temp\".
+
+    ' In this case
+    ' - check the User or User Group your web application is running under
+    ' - then add permissions for this User or User Group to read and write into that temp folder (c:\temp or c:\windows\temp\ folder)
+    ' - restart your web application and try again
+
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
