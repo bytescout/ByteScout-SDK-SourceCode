4234c9a9ee1f8225a7279b014d08510b03513872
estar73 <estarkov@gmail.com>
2019-08-01 08:29:44 +0000

    = regular update

---
+++ Sensitive Data Suite/C#/RemoveSensitiveDataFromRegion/Program.cs
@@ -0,0 +1,42 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using System.Drawing;
+using Bytescout.PDFExtractor;
+
+namespace RemoveSensitiveDataFromRegion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Bytescout.PDFExtractor.Remover instance
+            Remover remover = new Remover("demo", "demo");
+
+            // Load sample PDF document
+            remover.LoadDocumentFromFile(@"sample1.pdf");
+
+            // Remove text objects contained in the specified rectangle or intersecting with it.
+            // NOTE: The removed text might be larger than the specified rectangle. Currently the Remover is unable 
+            // to split PDF text objects.
+            remover.RemoveText(0, new RectangleF(45f, 101f, 183f, 310f), @"result1.pdf");
+
+            // Open output file in default application
+            System.Diagnostics.Process.Start("result1.pdf");
+
+            // Clean up.
+            remover.Dispose();
+        }
+    }
+}
---
+++ Sensitive Data Suite/VB.NET/RemoveSensitiveDataFromRegion/Module1.vb
@@ -0,0 +1,42 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Get free API Key https://app.pdf.co/signup                     '
+'                                                                                           '
+' Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                '
+' https://www.bytescout.com                                                                 '
+' https://www.pdf.co                                                                        '
+'*******************************************************************************************'
+
+
+Imports System.Drawing
+Imports Bytescout.PDFExtractor
+
+Class Program
+
+    Shared Sub Main(ByVal args As String())
+
+        ' Create Bytescout.PDFExtractor.Remover instance
+        Dim remover As New Remover("demo", "demo")
+
+        ' Load sample PDF document
+        remover.LoadDocumentFromFile("sample1.pdf")
+
+        ' Remove text objects contained in the specified rectangle or intersecting with it.
+        ' NOTE: The removed text might be larger than the specified rectangle. Currently the Remover is unable 
+        ' to split PDF text objects.
+        remover.RemoveText(0, New RectangleF(45.0F, 101.0F, 183.0F, 310.0F), "result1.pdf")
+
+        ' Clean up.
+        remover.Dispose()
+
+        Console.WriteLine()
+        Console.WriteLine("Press any key to continue and open result PDF files in default PDF viewer...")
+        Console.ReadKey()
+
+        Process.Start("result1.pdf")
+
+    End Sub
+
+End Class
