fe45f107d8e0e1d8f46e3cd0ba939e8c114ff4b9
estar73 <estarkov@gmail.com>
2019-08-02 02:33:32 +0000

    = regular update

---
+++ Sensitive Data Suite/C#/Remove Sensitive Text Using Regex Expressions/Program.cs
@@ -0,0 +1,61 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using System.Drawing;
+using Bytescout.PDFExtractor;
+
+namespace RemoveSensitiveTextUsingRegexExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Bytescout.PDFExtractor.Remover instance
+            Remover remover = new Remover("demo", "demo");
+
+            // Load sample PDF document
+            remover.LoadDocumentFromFile(@"samplePDF_EmailAddress.pdf");
+
+            // Prepare TextExtractor
+            using (TextExtractor textExtractor = new TextExtractor("demo", "demo"))
+            {
+                // Load document into TextExtractor
+                textExtractor.LoadDocumentFromFile(@"samplePDF_EmailAddress.pdf");
+
+                // Search email Addresses
+                // See the complete regular expressions reference at https://msdn.microsoft.com/en-us/library/az24scfc(v=vs.110).aspx
+                string regexPattern = @"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b";
+
+                // Enable RegexSearch
+                textExtractor.RegexSearch = true;
+
+                // Set word matching options
+                textExtractor.WordMatchingMode = WordMatchingMode.None;
+
+                ISearchResult[] searchResults = textExtractor.FindAll(0, regexPattern, caseSensitive: false);
+
+                // Remove text objects find by SearchResults.
+                // NOTE: The removed text might be larger than the specified rectangle. Currently the Remover is unable 
+                // to split PDF text objects.
+                remover.RemoveText(searchResults, @"result1.pdf");
+            }
+
+            // Open output file in default application
+            System.Diagnostics.Process.Start("result1.pdf");
+
+            // Clean up.
+            remover.Dispose();
+        }
+    }
+}
