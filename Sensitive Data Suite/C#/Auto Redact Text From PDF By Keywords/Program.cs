6dcafe0edc721b5e7a0f9622230610d8d355dc71
estar73 <estarkov@gmail.com>
2019-08-02 02:36:24 +0000

    = regular update

---
+++ Sensitive Data Suite/C#/Auto Redact Text From PDF By Keywords/Program.cs
@@ -0,0 +1,57 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using System.Drawing;
+using Bytescout.PDFExtractor;
+
+namespace AutoRedactTextFromPDFByKeywords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Bytescout.PDFExtractor.Remover instance
+            Remover remover = new Remover("demo", "demo");
+
+            // Load sample PDF document
+            remover.LoadDocumentFromFile(@"sample1.pdf");
+
+            // Search Keyword
+            string SearchKeyword = "Martian dichotomy";
+            
+            // Prepare TextExtractor
+            using (TextExtractor textExtractor = new TextExtractor("demo", "demo"))
+            {
+                // Load document into TextExtractor
+                textExtractor.LoadDocumentFromFile(@"sample1.pdf");
+
+                // Set word matching options
+                textExtractor.WordMatchingMode = WordMatchingMode.None;
+
+                ISearchResult[] searchResults = textExtractor.FindAll(0, SearchKeyword, caseSensitive: false);
+
+                // Remove text objects find by SearchResults.
+                // NOTE: The removed text might be larger than the specified rectangle. Currently the Remover is unable 
+                // to split PDF text objects.
+                remover.RemoveText(searchResults, @"result1.pdf");
+            }
+
+            // Open output file in default application
+            System.Diagnostics.Process.Start("result1.pdf");
+
+            // Clean up.
+            remover.Dispose();
+        }
+    }
+}
---
+++ Sensitive Data Suite/VB.NET/Auto Redact Text From PDF By Keywords/Module1.vb
@@ -0,0 +1,58 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Get free API Key https://app.pdf.co/signup                     '
+'                                                                                           '
+' Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                '
+' https://www.bytescout.com                                                                 '
+' https://www.pdf.co                                                                        '
+'*******************************************************************************************'
+
+
+Imports System.Drawing
+Imports Bytescout.PDFExtractor
+
+Class Program
+
+    Shared Sub Main(ByVal args As String())
+
+        ' Create Bytescout.PDFExtractor.Remover instance
+        Dim remover As New Remover("demo", "demo")
+
+        ' Load sample PDF document
+        remover.LoadDocumentFromFile("sample1.pdf")
+
+        ' Search Keyword
+        Dim SearchKeyword As String = "Martian dichotomy"
+
+        ' Prepare TextExtractor
+        Using textExtractor As New TextExtractor("demo", "demo")
+
+            ' Load document into TextExtractor
+            textExtractor.LoadDocumentFromFile("sample1.pdf")
+
+            ' Set word matching options
+            textExtractor.WordMatchingMode = WordMatchingMode.None
+
+            Dim searchResults() As ISearchResult = textExtractor.FindAll(0, SearchKeyword, caseSensitive:=False)
+
+            ' Remove text objects find by SearchResults.
+            ' NOTE: The removed text might be larger than the specified rectangle. Currently the Remover Is unable 
+            ' to split PDF text objects.
+            remover.RemoveText(searchResults, "result1.pdf")
+
+        End Using
+
+        ' Clean up.
+        remover.Dispose()
+
+        Console.WriteLine()
+        Console.WriteLine("Press any key to continue and open result PDF files in default PDF viewer...")
+        Console.ReadKey()
+
+        Process.Start("result1.pdf")
+
+    End Sub
+
+End Class
