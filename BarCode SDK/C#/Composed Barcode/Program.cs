a6a730d80fb0e033aea71dfc8f026d29bb2ecfd9
estar73 <estarkov@gmail.com>
2019-09-06 09:07:54 +0000

    = regular update

---
+++ BarCode SDK/C#/Composed Barcode/Program.cs
@@ -0,0 +1,53 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System.Diagnostics;
+using Bytescout.BarCode;
+using System.Drawing;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Result file
+            var resultFile = "result.png";
+
+            using (Barcode barcode = new Barcode("demo", "demo"))
+            {
+                barcode.Symbology = SymbologyType.Code128;
+
+                barcode.Margins = new Margins(0, 0, 0, 0);
+                barcode.DrawQuietZones = false;
+
+                barcode.Value = "123123";
+                Image image1 = barcode.GetImage();
+
+                barcode.Value = "123";
+                Image image2 = barcode.GetImage();
+
+                using (ImageComposer composer = new ImageComposer(innerGap: 10, margins: 5, compositionMode: CompositionMode.ArrangeHorizontally))
+                {
+                    composer.AddImage(image1);
+                    composer.AddImage(image2, 0, 0, rotationAngle: 90);
+
+                    composer.SaveComposedImage(resultFile);
+                }
+            }
+
+            // Open output file
+            Process.Start(resultFile);
+        }
+    }
+}
---
+++ BarCode SDK/VB.NET/Composed Barcode/Module1.vb
@@ -0,0 +1,47 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Get free API Key https://app.pdf.co/signup                     '
+'                                                                                           '
+' Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                '
+' https://www.bytescout.com                                                                 '
+' https://www.pdf.co                                                                        '
+'*******************************************************************************************'
+
+
+Imports System.Drawing
+Imports Bytescout.BarCode
+
+Module Module1
+
+    Sub Main()
+
+        Using barcode As New Barcode("demo", "demo")
+
+            barcode.Symbology = SymbologyType.Code128
+
+            barcode.Margins = New Margins(0, 0, 0, 0)
+            barcode.DrawQuietZones = False
+
+            barcode.Value = "123123"
+            Dim image1 As Image = barcode.GetImage()
+
+            barcode.Value = "123"
+            Dim image2 As Image = barcode.GetImage()
+
+            Using composer As New ImageComposer(10, 5, CompositionMode.ArrangeHorizontally)
+
+                composer.AddImage(image1)
+                composer.AddImage(image2, 0, 0, 90)
+
+                composer.SaveComposedImage("result.png")
+
+                ' Show image in default image viewer
+                Process.Start("result.png")
+
+            End Using
+        End Using
+    End Sub
+
+End Module
