ef8a2e8cdca698e8f80523a78337e0b08200626d
estar73 <estarkov@gmail.com>
2019-09-11 05:35:11 +0000

    = regular update

---
+++ BarCode Reader SDK/C#/Inspect Decoded Barcodes/Program.cs
@@ -0,0 +1,71 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using System.Drawing.Imaging;
+using System.IO;
+using Bytescout.BarCodeReader;
+
+namespace InspectDecodedBarcodes
+{
+    class Program
+    {
+        static int i = 0;
+        const string ImageFile = "Code_39.png";
+
+        static void Main()
+        {
+            Console.WriteLine("Reading barcode(s) from image {0}", Path.GetFullPath(ImageFile));
+
+            Reader reader = new Reader();
+            reader.RegistrationName = "demo";
+			reader.RegistrationKey = "demo";
+
+            // Set barcode type to find
+            reader.BarcodeTypesToFind.Code39 = true;
+
+            reader.InspectDecodedBarcodes += Reader_InspectDecodedBarcodes;
+
+            // Read barcodes
+            FoundBarcode[] barcodes = reader.ReadFrom(ImageFile);
+
+            foreach (FoundBarcode barcode in barcodes)
+            {
+                Console.WriteLine("Found barcode with type '{0}' and value '{1}'", barcode.Type, barcode.Value);
+            }
+			
+			// Cleanup
+			reader.Dispose();
+
+            Console.WriteLine("Press any key to exit..");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Inspect decoded barcode event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="image"></param>
+        private static void Reader_InspectDecodedBarcodes(object sender, System.Drawing.Image image)
+        {
+            var imageFile = $"page{++i}.png";
+            image.Save(imageFile, ImageFormat.Png);
+
+            Console.WriteLine($"Inspected image file saved as ${imageFile}");
+
+            // You should explicitly dispose the image object to avoid resource leaks.
+            image.Dispose();
+
+        }
+    }
+}
---
+++ BarCode Reader SDK/VB.NET/Inspect Decoded Barcodes/Module1.vb
@@ -0,0 +1,66 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Get free API Key https://app.pdf.co/signup                     '
+'                                                                                           '
+' Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                '
+' https://www.bytescout.com                                                                 '
+' https://www.pdf.co                                                                        '
+'*******************************************************************************************'
+
+
+Imports System.Drawing
+Imports System.Drawing.Imaging
+Imports System.IO
+Imports Bytescout.BarCodeReader
+
+Module Module1
+
+    Dim i As Int32 = 0
+
+    Sub Main()
+
+        Const imageFile As String = "Code_39.png"
+
+        Console.WriteLine("Reading barcode(s) from image {0}", Path.GetFullPath(imageFile))
+
+        Dim reader As New Reader()
+        reader.RegistrationName = "demo"
+        reader.RegistrationKey = "demo"
+
+        ' Set barcode type to find
+        reader.BarcodeTypesToFind.Code39 = True
+
+        ' Handle Barcode Reader's Inspect Decoded Barcodes event
+        AddHandler reader.InspectDecodedBarcodes, AddressOf Reader_InspectDecodedBarcodes
+
+        ' Read barcodes
+        Dim barcodes As FoundBarcode() = reader.ReadFrom(imageFile)
+
+        For Each barcode As FoundBarcode In barcodes
+            Console.WriteLine("Found barcode with type '{0}' and value '{1}'", barcode.Type, barcode.Value)
+        Next
+
+        ' Cleanup
+        reader.Dispose()
+
+        Console.WriteLine("Press any key to exit..")
+        Console.ReadKey()
+
+    End Sub
+
+    ''' <summary>
+    ''' Inspect decoded barcode event handler
+    ''' </summary>
+    Private Sub Reader_InspectDecodedBarcodes(sender As Object, image As Image)
+        Dim imageName = "page" + (++i).ToString() + ".png"
+        image.Save(imageName, ImageFormat.Png)
+
+        Console.WriteLine("Inspected barcode saved as " + imageName)
+
+        ' You should explicitly dispose the image object to avoid resource leaks.
+        image.Dispose()
+    End Sub
+
+End Module
