93a380721921e8ac3a5d822505a6418afabc69fa
estar73 <estarkov@gmail.com>
2019-10-07 05:30:06 +0000

    = regular update

---
+++ BarCode Reader SDK/C#/Read 50 datamatrix barcodes from picture/Program.cs
@@ -0,0 +1,53 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using Bytescout.BarCodeReader;
+
+namespace Read50DataMatrixBarcocesFromPicture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Create and activate Bytescout.BarCodeReader.Reader instance
+                using (Reader reader = new Reader("demo", "demo"))
+                {
+                    // Set barcode type to find
+                    reader.BarcodeTypesToFind.DataMatrix = true;
+
+                    /* -----------------------------------------------------------------------
+                    NOTE: We can read barcodes from specific page to increase performance.
+                    For sample please refer to "Decoding barcodes from PDF by pages" program.
+                    ----------------------------------------------------------------------- */
+
+                    // Read barcodes
+                    FoundBarcode[] barcodes = reader.ReadFrom("50_DataMatrix.png");
+
+                    foreach (FoundBarcode code in barcodes)
+                        Console.WriteLine("Found barcode value: '{1}'", code.Type, code.Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
