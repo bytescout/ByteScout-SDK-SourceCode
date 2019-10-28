b45172a360237ba206b55f7cd770eec10b077888
estar73 <estarkov@gmail.com>
2019-10-28 04:01:45 +0000

    = regular update

---
+++ BarCode Reader SDK/VBScript/Read barcode From PDF With OrientationType/TestBarcodeReadingFromPDF.vbs
@@ -0,0 +1,29 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Get free API Key https://app.pdf.co/signup                     '
+'                                                                                           '
+' Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                '
+' https://www.bytescout.com                                                                 '
+' https://www.pdf.co                                                                        '
+'*******************************************************************************************'
+
+
+Set bc = CreateObject("Bytescout.BarCodeReader.Reader")
+
+' Set Barcode Types to Find
+bc.BarcodeTypesToFind.PatchCode = True
+
+' Add vertical orientations to analysis
+bc.Orientation = 17 Or 2 Or 32768
+
+' Reading barcode from image file
+bc.ReadFromFile "PatchCode.png"
+
+' Iterate throuhg all found barcodes
+For i = 0 To bc.FoundCount - 1
+     Msgbox "Found barcode with type " & Cstr(bc.GetFoundBarcodeType(i)) & " and value " & bc.GetFoundBarcodeValue(i)
+Next
+
+Set bc = Nothing
---
+++ Document Parser SDK/C#/Parse and Generate HL7 Output/Program.cs
@@ -0,0 +1,51 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using System.Diagnostics;
+using ByteScout.DocumentParser;
+
+// This example demonstrates document data parsing to JSON, YAML and XML formats.
+
+namespace HL7CreationFromJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1: Generate Parse PDF File With Template and Generate Json
+            string inputPDF = "InputData/Test_Report_Format.pdf";
+            string template = "InputData/TestReportFormat.yml";
+
+            // Create Document Parser Instance
+            DocumentParser docParser = new DocumentParser("demo", "demo");
+
+            // Add Template
+            docParser.AddTemplate(template);
+
+            // Parse document data in JSON format
+            string jsonString = docParser.ParseDocument(inputPDF, OutputFormat.JSON);
+
+            // Step 2: Parse Json fileds in class format
+            var oInpModel = JsonParserHelper.ParseJsonHL7Fields(jsonString);
+
+            // Step 3: Get Data in HL7 Format
+            var oHL7Format = Hl7Helper.GetHL7Format(oInpModel);
+
+            // Store HL7 File and open with default associated program
+            var outputFile = "outputHl7.txt";
+            System.IO.File.WriteAllText(outputFile, oHL7Format);
+            Process.Start(outputFile);
+        }
+    }
+}
