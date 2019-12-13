3f18ed22923c60f1ce41468761e4bccd51ca8a8e
estar73 <estarkov@gmail.com>
2019-12-13 14:05:24 +0000

    = regular update

---
+++ BarCode Reader SDK/C#/Set Orientation and Read Rotated Barcodes/Program.cs
@@ -0,0 +1,59 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using System.IO;
+using Bytescout.BarCodeReader;
+
+namespace ReadRotatedBarcodes
+{
+    class Program
+    {
+        const string ImageFile = "barcode_skewed2.png";
+
+        static void Main()
+        {
+            Console.WriteLine("Reading barcode(s) from image {0}", Path.GetFullPath(ImageFile));
+
+            Reader reader = new Reader();
+            reader.RegistrationName = "demo";
+			reader.RegistrationKey = "demo";
+
+            // Set barcode type to find
+            reader.BarcodeTypesToFind.Code128 = true;
+
+            // Set barcode Orientation
+            // DiagonalFromTopLeftToBottomRight11 - Barcodes rotated clockwise by about 11 deg. 
+            reader.Orientation = OrientationType.DiagonalFromTopLeftToBottomRight11;
+
+            /* -----------------------------------------------------------------------
+            NOTE: We can read barcodes from specific page to increase performance.
+            For sample please refer to "Decoding barcodes from PDF by pages" program.
+            ----------------------------------------------------------------------- */
+
+            // Read barcodes
+            FoundBarcode[] barcodes = reader.ReadFrom(ImageFile);
+
+            foreach (FoundBarcode barcode in barcodes)
+            {
+                Console.WriteLine("Found barcode with type '{0}' and value '{1}'", barcode.Type, barcode.Value);
+            }
+
+            // Cleanup
+            reader.Dispose();
+
+            Console.WriteLine("Press any key to exit..");
+            Console.ReadKey();
+        }
+    }
+}
