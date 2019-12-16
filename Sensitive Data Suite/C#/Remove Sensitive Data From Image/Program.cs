b110007c974e3bc67c23e8b6420bbd2f0742398f
estar73 <estarkov@gmail.com>
2019-12-16 09:21:15 +0000

    = regular update

---
+++ Sensitive Data Suite/C#/Remove Sensitive Data From Image/Program.cs
@@ -0,0 +1,73 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using Bytescout.PDFExtractor;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace RemoveSensitiveDataFromImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = @"scanned_sample_EmailSSN.png";
+            string result = @"result.png";
+
+            using (SensitiveDataDetector detector = new SensitiveDataDetector("demo", "demo"))
+            {
+                // Enable OCR mode and provide path to data folder
+                detector.OCRMode = OCRMode.Auto;
+                detector.OCRLanguageDataFolder = @"c:\Program Files\Bytescout PDF Extractor SDK\ocrdata\";
+
+                detector.LoadDocumentFromFile(input);
+
+                // Detect sensitive data
+                SensitiveDataDetectionResults detectionResults = detector.PerformDetection();
+
+                if (detectionResults.DetectedItems.Length > 0)
+                {
+                    using (var image = Image.FromFile(input))
+                    {
+                        // Create new bitmap, as graphics object require indexed pixel format
+                        using (var newBitmap = new Bitmap(image.Width, image.Height))
+                        {
+                            using (Graphics g = Graphics.FromImage(newBitmap))
+                            {
+                                g.DrawImage(image, 0, 0);
+
+                                foreach (var detectedItem in detectionResults.DetectedItems)
+                                {
+                                    // Convert coordinates from Points (1/72") to display pixels
+                                    Rectangle rect = new Rectangle(
+                                        (int)(detectedItem.Rectangle.X * 96 / 72),
+                                        (int)(detectedItem.Rectangle.Y * 96 / 72),
+                                        (int)(detectedItem.Rectangle.Width * 96 / 72),
+                                        (int)(detectedItem.Rectangle.Height * 96 / 72));
+
+                                    // Paint over the detected item with a white or black brush
+                                    g.FillRectangle(Brushes.Black, rect);
+                                }
+                            }
+
+                            newBitmap.Save(result);
+                        }
+                    }
+
+                    // Open output file with attached application
+                    Process.Start(result);
+                }
+            }
+        }
+    }
+}
---
+++ Sensitive Data Suite/VB.NET/Remove Sensitive Data From Image/Module1.vb
@@ -0,0 +1,78 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Get free API Key https://app.pdf.co/signup                     '
+'                                                                                           '
+' Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                '
+' https://www.bytescout.com                                                                 '
+' https://www.pdf.co                                                                        '
+'*******************************************************************************************'
+
+
+Imports System.IO
+Imports System.Drawing
+Imports Bytescout.PDFExtractor
+
+Class Program
+
+    Shared Sub Main(ByVal args As String())
+
+        Dim input As String = "scanned_sample_EmailSSN.png"
+        Dim result As String = "result.png"
+
+        Using detector As New SensitiveDataDetector("demo", "demo")
+
+            ' Enable OCR mode And provide path to data folder
+            detector.OCRMode = OCRMode.Auto
+            detector.OCRLanguageDataFolder = "c:\Program Files\Bytescout PDF Extractor SDK\ocrdata\"
+
+            detector.LoadDocumentFromFile(input)
+
+            ' Detect sensitive data
+            Dim detectionResults As SensitiveDataDetectionResults = detector.PerformDetection()
+
+            If detectionResults.DetectedItems.Length > 0 Then
+
+                Using image As Image = Image.FromFile(input)
+
+                    ' Create new bitmap, as graphics object require indexed pixel format 
+                    Using newBitmap As New Bitmap(image.Width, image.Height)
+
+                        Using g As Graphics = Graphics.FromImage(newBitmap)
+
+                            g.DrawImage(image, 0, 0)
+
+                            For Each detectedItem As DetectedSensitiveItem In detectionResults.DetectedItems
+
+                                ' Convert coordinates from Points (1/72") to display pixels
+
+                                Dim rect As Rectangle = New Rectangle(
+                                        CInt(detectedItem.Rectangle.X * 96 / 72),
+                                        CInt(detectedItem.Rectangle.Y * 96 / 72),
+                                        CInt(detectedItem.Rectangle.Width * 96 / 72),
+                                        CInt(detectedItem.Rectangle.Height * 96 / 72))
+
+                                ' Paint over the detected item with a white Or black brush
+                                g.FillRectangle(Brushes.Black, rect)
+
+                            Next
+
+                        End Using
+
+                        newBitmap.Save(result)
+
+                    End Using
+
+                End Using
+
+            End If
+
+        End Using
+
+        ' Open output file with attached application
+        Process.Start(result)
+
+    End Sub
+
+End Class
