80e1ed6bc3f4c2feee47518132e95b89b943984e
estar73 <estarkov@gmail.com>
2020-01-20 05:27:22 +0000

    = regular update

---
+++ BarCode Reader SDK/C#/Decode Macro PDF 417/Program.cs
@@ -0,0 +1,172 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Bytescout.BarCodeReader;
+
+namespace ReadMacroPDF417
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Input Image files
+            var ImageFiles = new string[] { "MacroPDFBarcode-part1.png", "MacroPDFBarcode-part2.png", "MacroPDFBarcode-part3.png" };
+
+            // Create Instance of Macro PDF 417 class
+            var oMacroPDF417Values = new MacroPDF417Values();
+
+            // Loop through input files to get value
+            foreach (string itmInput in ImageFiles)
+            {
+                using (var reader = new Reader("demo", "demo"))
+                {
+                    /* -----------------------------------------------------------------------
+                   NOTE: We can read barcodes from specific page to increase performance.
+                   For sample please refer to "Decoding barcodes from PDF by pages" program.
+                   ----------------------------------------------------------------------- */
+                    // Set barcode type to find
+                    reader.BarcodeTypesToFind.PDF417 = true;
+
+                    // Read barcodes
+                    Console.WriteLine("Reading barcode(s) from image {0}", Path.GetFullPath(itmInput));
+                    FoundBarcode[] barcodes = reader.ReadFrom(itmInput);
+
+                    foreach (FoundBarcode barcode in barcodes)
+                    {
+                        Console.WriteLine("Found Value: " + barcode.Value + "\n");
+
+                        // Add found barcode value
+                        oMacroPDF417Values.AddValue(barcode.Value);
+                    }
+                }
+            }
+
+            // Get MacroPDF417 output value
+            var outputValue = oMacroPDF417Values.GetValue();
+
+            Console.WriteLine("\n\nConsolidated Output Value: " + outputValue);
+
+            Console.WriteLine("\n\nPress any key to exit..");
+            Console.ReadKey();
+        }
+    }
+
+    public class MacroPDF417Values
+    {
+        #region Constructors and Properties
+
+        public MacroPDF417Values() { lstSegments = new List<MacroPDFSegmment>(); }
+
+        public List<MacroPDFSegmment> lstSegments { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Add Value
+        /// </summary>
+        public void AddValue(string strValue)
+        {
+            lstSegments.Add(new MacroPDFSegmment(strValue));
+        }
+
+        /// <summary>
+        /// Get value
+        /// </summary>
+        public string GetValue()
+        {
+            var retValue = new StringBuilder("");
+
+            foreach (var itmSegment in lstSegments.Where(x => x.SegmentNo > 0).OrderBy(x => x.SegmentNo))
+            {
+                retValue.Append(itmSegment.SegmentValue);
+            }
+
+            return retValue.ToString();
+        }
+
+        #endregion
+
+    }
+
+    public class MacroPDFSegmment
+    {
+        #region Constructors and Declarations
+
+        public MacroPDFSegmment() { }
+        public MacroPDFSegmment(string inpValue)
+        {
+            if (!string.IsNullOrEmpty(inpValue))
+            {
+                Segments = inpValue.Split('\\');
+            }
+        }
+
+        private string[] Segments { get; set; }
+
+        /// <summary>
+        /// Get Segment Index
+        /// </summary>
+        public int SegmentNo
+        {
+            get
+            {
+                int retSegmentValue = -1;
+                var segmentVal = GetSegmentValue(2);
+
+                if (!string.IsNullOrEmpty(segmentVal))
+                {
+                    int.TryParse(segmentVal, out retSegmentValue);
+                }
+
+                return retSegmentValue;
+            }
+        }
+
+        /// <summary>
+        /// Get Segment Value
+        /// </summary>
+        public string SegmentValue
+        {
+            get
+            {
+                return GetSegmentValue(1);
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Get Segment Value
+        /// </summary>
+        public string GetSegmentValue(int segmentNo)
+        {
+            if (Segments != null && Segments.Length >= segmentNo)
+            {
+                return Segments[segmentNo - 1];
+            }
+
+            return "";
+        }
+
+        #endregion
+    }
+}
---
+++ BarCode Reader SDK/VB.NET/Decode Macro PDF 417/Module1.vb
@@ -0,0 +1,144 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Get free API Key https://app.pdf.co/signup                     '
+'                                                                                           '
+' Copyright © 2017-2019 ByteScout, Inc. All rights reserved.                                '
+' https://www.bytescout.com                                                                 '
+' https://www.pdf.co                                                                        '
+'*******************************************************************************************'
+
+
+Imports System.IO
+Imports System.Linq
+Imports System.Text
+Imports Bytescout.BarCodeReader
+
+
+Module Module1
+
+    Sub Main()
+
+        ' Input Image files
+        Dim ImageFiles = New String() {"MacroPDFBarcode-part1.png", "MacroPDFBarcode-part2.png", "MacroPDFBarcode-part3.png"}
+
+        ' Create Instance of Macro PDF 417 class
+        Dim oMacroPDF417Values = New MacroPDF417Values()
+
+        ' Loop through input files to get value
+        For Each itmInput As String In ImageFiles
+            Using reader = New Reader("demo", "demo")
+
+                ' -----------------------------------------------------------------------
+                ' NOTE: We can read barcodes from specific page to increase performance.
+                ' For sample please refer To "Decoding barcodes from PDF by pages" program.
+                '  -----------------------------------------------------------------------
+
+                ' Set barcode type to find
+                reader.BarcodeTypesToFind.PDF417 = True
+
+                ' Read barcodes
+                Console.WriteLine("Reading barcode(s) from image {0}", Path.GetFullPath(itmInput))
+                Dim barcodes As FoundBarcode() = reader.ReadFrom(itmInput)
+
+                For Each barcode As FoundBarcode In barcodes
+                    Console.WriteLine("Found Value: " & barcode.Value & vbLf)
+                    oMacroPDF417Values.AddValue(barcode.Value)
+                Next
+
+            End Using
+        Next
+
+        ' Get MacroPDF417 output value
+        Dim outputValue = oMacroPDF417Values.GetValue()
+        Console.WriteLine(vbLf & vbLf & "Consolidated Output Value: " & outputValue)
+
+        Console.WriteLine(vbLf & vbLf & "Press any key to exit..")
+        Console.ReadKey()
+
+    End Sub
+
+
+    Public Class MacroPDF417Values
+
+        Public Sub New()
+            lstSegments = New List(Of MacroPDFSegmment)()
+        End Sub
+
+        Public Property lstSegments As List(Of MacroPDFSegmment)
+
+        ''' <summary>
+        ''' Add Value
+        ''' </summary>
+        Public Sub AddValue(ByVal strValue As String)
+            lstSegments.Add(New MacroPDFSegmment(strValue))
+        End Sub
+
+        ''' <summary>
+        ''' Get value
+        ''' </summary>
+        Public Function GetValue() As String
+            Dim retValue = New StringBuilder("")
+
+            For Each itmSegment As MacroPDFSegmment In lstSegments.Where(Function(x) x.SegmentNo > 0).OrderBy(Function(x) x.SegmentNo)
+                retValue.Append(itmSegment.SegmentValue)
+            Next
+
+            Return retValue.ToString()
+        End Function
+
+    End Class
+
+    Public Class MacroPDFSegmment
+
+        Public Sub New()
+        End Sub
+
+        Public Sub New(ByVal inpValue As String)
+            If Not String.IsNullOrEmpty(inpValue) Then
+                Segments = inpValue.Split("\"c)
+            End If
+        End Sub
+
+        Private Property Segments As String()
+
+        ''' <summary>
+        ''' Get Segment Index
+        ''' </summary>
+        Public ReadOnly Property SegmentNo As Integer
+            Get
+                Dim retSegmentValue As Integer = -1
+                Dim segmentVal = GetSegmentValue(2)
+
+                If Not String.IsNullOrEmpty(segmentVal) Then
+                    Integer.TryParse(segmentVal, retSegmentValue)
+                End If
+
+                Return retSegmentValue
+            End Get
+        End Property
+
+        ''' <summary>
+        ''' Get Segment Value
+        ''' </summary>
+        Public ReadOnly Property SegmentValue As String
+            Get
+                Return GetSegmentValue(1)
+            End Get
+        End Property
+
+        ''' <summary>
+        ''' Get Segment Value
+        ''' </summary>
+        Public Function GetSegmentValue(ByVal segmentNo As Integer) As String
+            If Segments IsNot Nothing AndAlso Segments.Length >= segmentNo Then
+                Return Segments(segmentNo - 1)
+            End If
+
+            Return ""
+        End Function
+    End Class
+
+
+End Module
