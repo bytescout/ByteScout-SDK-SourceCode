cdb36bc0c8ad74d1cbe50fc1646ebd0f757c63a5
estar73 <estarkov@gmail.com>
2020-02-07 05:23:50 +0000

    = regular update

---
+++ PDF Extractor SDK/C#/Load Unicode CSV to DataTable using OLEDB/Program.cs
@@ -0,0 +1,64 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using Bytescout.PDFExtractor;
+using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Globalization;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputDocument = Path.GetFullPath(@".\UnicodeSample.pdf");
+            string csvFilePath = Path.ChangeExtension(inputDocument, ".csv");
+            string csvFileName = Path.GetFileName(csvFilePath);
+            string csvDirectory = Path.GetDirectoryName(Path.GetFullPath(csvFilePath));
+
+            // Create Bytescout.PDFExtractor.CSVExtractor instance
+            using (CSVExtractor extractor = new CSVExtractor("demo", "demo"))
+            {
+                extractor.LoadDocumentFromFile(inputDocument);
+
+                extractor.CSVSeparatorSymbol = ",";
+
+                string csvText = extractor.GetCSV();
+            
+                // Save csv text in UTF-8 encoding without BOM (byte order mark):
+                File.WriteAllText(csvFilePath, csvText);
+            }
+
+            // Please Note: Target the project to x86 because Microsoft.Jet.OLEDB.4.0 driver is 32-bit only.
+            using (OleDbConnection connection = new OleDbConnection($@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=""{csvDirectory}"";Extended Properties=""Text;FMT=$;HDR=No;CharacterSet=65001"""))
+            {
+                using (OleDbCommand command = new OleDbCommand($"select * from [{csvFileName}]", connection))
+                {
+                    using (OleDbDataAdapter adapter = new OleDbDataAdapter(command))
+                    {
+                        DataTable table = new DataTable();
+                        table.Locale = CultureInfo.CurrentCulture;
+                        adapter.Fill(table);
+
+                        Console.WriteLine($"Loaded {table.Rows.Count} lines.");
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PDF Extractor SDK/VB.NET/Load Unicode CSV to DataTable using OLEDB/Module1.vb
@@ -0,0 +1,65 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Get free API Key https://app.pdf.co/signup                     '
+'                                                                                           '
+' Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                '
+' https://www.bytescout.com                                                                 '
+' https://www.pdf.co                                                                        '
+'*******************************************************************************************'
+
+
+Imports System.Data.OleDb
+Imports System.Globalization
+Imports System.IO
+Imports Bytescout.PDFExtractor
+
+Namespace ConsoleApplication1
+
+    Class Program
+
+        Shared Sub Main(ByVal args As String())
+
+            Dim inputDocument As String = Path.GetFullPath(".\UnicodeSample.pdf")
+            Dim csvFilePath As String = Path.ChangeExtension(inputDocument, ".csv")
+            Dim csvFileName As String = Path.GetFileName(csvFilePath)
+            Dim csvDirectory As String = Path.GetDirectoryName(Path.GetFullPath(csvFilePath))
+
+            ' Create Bytescout.PDFExtractor.CSVExtractor instance
+            Using extractor As CSVExtractor = New CSVExtractor("demo", "demo")
+
+                extractor.LoadDocumentFromFile(inputDocument)
+
+                extractor.CSVSeparatorSymbol = ","
+
+                Dim csvText As String = extractor.GetCSV()
+
+                ' Save csv text in UTF-8 encoding without BOM (byte order mark):
+                File.WriteAllText(csvFilePath, csvText)
+
+            End Using
+
+            ' Please Note: Target the project to x86 because Microsoft.Jet.OLEDB.4.0 driver is 32-bit only.
+            Using connection As OleDbConnection = New OleDbConnection($"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=""{csvDirectory}"";Extended Properties=""Text;FMT=$;HDR=No;CharacterSet=65001""")
+
+                Using command As OleDbCommand = New OleDbCommand($"select * from [{csvFileName}]", connection)
+
+                    Using adapter As OleDbDataAdapter = New OleDbDataAdapter(command)
+                        Dim table As DataTable = New DataTable()
+                        table.Locale = CultureInfo.CurrentCulture
+                        adapter.Fill(table)
+                        Console.WriteLine($"Loaded {table.Rows.Count} lines.")
+                    End Using
+
+                End Using
+
+            End Using
+
+            Console.ReadKey()
+
+        End Sub
+
+    End Class
+End Namespace
+
