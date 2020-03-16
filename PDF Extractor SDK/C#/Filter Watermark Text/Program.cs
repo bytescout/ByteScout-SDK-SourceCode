329fd205edfc1c342b37bb109dfa89f7bdf577a2
estar73 <estarkov@gmail.com>
2020-03-16 09:37:06 +0000

    = regular update

---
+++ PDF Extractor SDK/C#/Filter Watermark Text/Program.cs
@@ -0,0 +1,49 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System.Diagnostics;
+using System.Drawing;
+using Bytescout.PDFExtractor;
+
+namespace ExtractText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Bytescout.PDFExtractor.TextExtractor instance
+            TextExtractor extractor = new TextExtractor();
+            extractor.RegistrationName = "demo";
+            extractor.RegistrationKey = "demo";
+
+            // Load sample PDF document
+            extractor.LoadDocumentFromFile(@".\DocumentWithWatermark.pdf");
+
+            // Filter text using text filter
+            extractor.AddFilter(@"^COPY$", caseSensitive: true, useRegex: true);
+
+            // Filter text using appearance filter
+            // extractor.AddFilter("Arial", fontSize: 203, exclude: true);
+
+            // Save extracted text to file
+            extractor.SaveTextToFile(@".\result.txt");
+
+            // Cleanup
+            extractor.Dispose();
+
+            // Open result file in default associated application
+            ProcessStartInfo processStartInfo = new ProcessStartInfo(@".\result.txt");
+            Process.Start(processStartInfo);
+        }
+    }
+}
---
+++ PDF Extractor SDK/VB.NET/Filter Watermark Text/Program.vb
@@ -0,0 +1,41 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Get free API Key https://app.pdf.co/signup                     '
+'                                                                                           '
+' Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                '
+' https://www.bytescout.com                                                                 '
+' https://www.pdf.co                                                                        '
+'*******************************************************************************************'
+
+
+Imports Bytescout.PDFExtractor
+
+Class Program
+	Friend Shared Sub Main(args As String())
+
+		' Create Bytescout.PDFExtractor.TextExtractor instance
+		Dim extractor As New TextExtractor()
+		extractor.RegistrationName = "demo"
+		extractor.RegistrationKey = "demo"
+
+		' Load sample PDF document
+		extractor.LoadDocumentFromFile(".\DocumentWithWatermark.pdf")
+
+        ' Filter text using text filter
+        extractor.AddFilter("^COPY$", caseSensitive:=True, useRegex:=True)
+
+        ' Filter text using appearance filter
+        ' extractor.AddFilter("Arial", fontSize:=203, exclude:=True)
+
+        ' Save extracted text to file
+        extractor.SaveTextToFile(".\result.txt")
+
+		' Cleanup
+		extractor.Dispose()
+
+		' Open result file in default associated application (for demo purposes)
+		System.Diagnostics.Process.Start(".\result.txt")
+	End Sub
+End Class
