22e549111b92f63b8d8fc175cec0fbd7c572ed4f
estar73 <estarkov@gmail.com>
2020-03-30 03:08:18 +0000

    = regular update

---
+++ Premium Suite/C#/Generate gs1-128 barcode with barcode sdk/Program.cs
@@ -0,0 +1,39 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using Bytescout.BarCode;
+using System.Diagnostics;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create new barcode
+            Barcode barcode = new Barcode();
+
+            // Set symbology
+            barcode.Symbology = SymbologyType.GS1_128;
+            
+            // Set value            
+            barcode.Value = "(12)this is a test";
+
+            // Save barcode to image
+            barcode.SaveImage("result.png");
+
+            // Show image in default image viewer
+            Process.Start("result.png");
+        }
+    }
+}
