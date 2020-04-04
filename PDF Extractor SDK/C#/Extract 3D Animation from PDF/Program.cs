25788c52a59b090f22bac76294f9c5a3b84f9784
estar73 <estarkov@gmail.com>
2020-04-04 10:45:14 +0000

    = regular update

---
+++ PDF Extractor SDK/C#/Extract 3D Animation from PDF/Program.cs
@@ -0,0 +1,52 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using Bytescout.PDFExtractor;
+
+namespace Extract3DAnimation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Bytescout.PDFExtractor.MultimediaExtractor instance
+            MultimediaExtractor extractor = new MultimediaExtractor();
+            extractor.RegistrationName = "demo";
+            extractor.RegistrationKey = "demo";
+            
+            // Load PDF document
+            // (!) We do not provide the sample document, please load your own.
+            extractor.LoadDocumentFromFile(@"sample.pdf");
+
+            int i = 0;
+
+            // Initialize 3D animations enumeration
+            if (extractor.GetFirst3D())
+            {
+                do
+                {
+                    string outputFileName = "3d_animation" + i + extractor.GetCurrent3DExtension();
+
+                    // Save 3D animation to file
+                    extractor.SaveCurrent3DToFile(outputFileName);
+
+                    i++;
+                } 
+                while (extractor.GetNext3D()); // Advance enumeration
+            }
+
+            // Cleanup
+			extractor.Dispose();
+        }
+    }
+}
---
+++ PDF Extractor SDK/VB.NET/Extract 3D Animation from PDF/Program.vb
@@ -0,0 +1,46 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Get free API Key https://app.pdf.co/signup                     '
+'                                                                                           '
+' Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                '
+' https://www.bytescout.com                                                                 '
+' https://www.pdf.co                                                                        '
+'*******************************************************************************************'
+
+
+Imports Bytescout.PDFExtractor
+
+Class Program
+    Friend Shared Sub Main(ByVal args As String())
+
+        ' Create Bytescout.PDFExtractor.MultimediaExtractor instance
+        Dim extractor As New MultimediaExtractor()
+		extractor.RegistrationName = "demo"
+		extractor.RegistrationKey = "demo"
+
+        ' Load PDF document
+        ' (!) We do not provide the sample document, please load your own.
+        extractor.LoadDocumentFromFile("sample.pdf")
+
+        Dim i As Integer = 0
+
+        ' Initialize 3D animations enumeration
+        If extractor.GetFirst3D() Then
+            Do
+                Dim outputFileName As String = "movie" & i & extractor.GetCurrent3DExtension()
+
+                ' Save 3D animation to file
+                extractor.SaveCurrent3DToFile(outputFileName)
+
+                i = i + 1
+
+            Loop While extractor.GetNext3D() ' Advance enumeration
+        End If
+
+        ' Cleanup
+		extractor.Dispose()
+
+    End Sub
+End Class
