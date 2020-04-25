703297ff75c0f2e40a0ca99608c3fd3113677cad
estar73 <estarkov@gmail.com>
2020-04-25 05:05:05 +0000

    = regular update

---
+++ Text Recognition SDK/C#/Save OCR Objects As JSON/Program.cs
@@ -0,0 +1,60 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using System;
+using System.Diagnostics;
+using ByteScout.TextRecognition;
+
+namespace SaveOcrObjectAsJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputDocument = @".\ocr-sample.pdf";
+            string outputDocument = @".\result.json";
+
+            // Create and activate TextRecognizer instance
+            using (TextRecognizer textRecognizer = new TextRecognizer("demo", "demo"))
+            {
+                try
+                {
+                    // Load document (image or PDF)
+                    textRecognizer.LoadDocument(inputDocument);
+
+                    // Set the location of OCR language data files
+                    textRecognizer.OCRLanguageDataFolder = @"c:\Program Files\ByteScout Text Recognition SDK\ocrdata_best\";
+
+                    // Set OCR language.
+                    // "eng" for english, "deu" for German, "fra" for French, "spa" for Spanish, etc. - according to files in "ocrdata" folder
+                    // Find more language files at https://github.com/bytescout/ocrdata
+                    textRecognizer.OCRLanguage = "eng"; 
+                                    
+                    // Recognize text from page and save each ocr word object to json
+                    textRecognizer.SaveOCRObjectsAsJSON(outputDocument, 0, OCRObjectType.Word);
+
+                    // Open the result file in default associated application (for demo purposes)
+                    Process.Start(outputDocument);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Text Recognition SDK/VB.NET/Save OCR Objects As XML/Module1.vb
@@ -0,0 +1,55 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Get free API Key https://app.pdf.co/signup                     '
+'                                                                                           '
+' Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                '
+' https://www.bytescout.com                                                                 '
+' https://www.pdf.co                                                                        '
+'*******************************************************************************************'
+
+
+Imports System
+Imports System.Diagnostics
+Imports Bytescout.TextRecognition
+
+Module Module1
+
+    Sub Main()
+
+        Dim inputDocument As String = ".\ocr-sample.pdf"
+        Dim outputDocument As String = ".\result.xml"
+
+        ' Create and activate TextRecognizer instance
+        Using textRecognizer As TextRecognizer = New TextRecognizer("demo", "demo")
+
+            Try
+                ' Load document (image or PDF)
+                textRecognizer.LoadDocument(inputDocument)
+
+                ' Set the location of OCR language data files
+                textRecognizer.OCRLanguageDataFolder = "c:\Program Files\ByteScout Text Recognition SDK\ocrdata_best\"
+
+                ' Set OCR language.
+                ' "eng" for english, "deu" for German, "fra" for French, "spa" for Spanish, etc. - according to files in "ocrdata" folder
+                ' Find more language files at https://github.com/bytescout/ocrdata
+                textRecognizer.OCRLanguage = "eng"
+
+                ' Recognize text from page and save objects as word to xml file
+                textRecognizer.SaveOCRObjectsAsXML(outputDocument, 0, OCRObjectType.Word)
+
+                ' Open the result file in default associated application (for demo purposes)
+                Process.Start(outputDocument)
+
+            Catch exception As Exception
+
+                Console.WriteLine(exception)
+
+            End Try
+
+        End Using
+
+    End Sub
+
+End Module
