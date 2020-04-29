e566367d6758d257a5ef36f0144648ba45f64f03
estar73 <estarkov@gmail.com>
2020-04-29 05:44:30 +0000

    = regular update

---
+++ PDF Extractor SDK/C#/PDF To XLSX by pages/Program.cs
@@ -0,0 +1,57 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using Bytescout.PDFExtractor;
+using System;
+
+namespace PDF2XLS
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Bytescout.PDFExtractor.XLSExtractor instance
+            XLSExtractor extractor = new XLSExtractor();
+            extractor.RegistrationName = "demo";
+            extractor.RegistrationKey = "demo";
+
+            // Load sample PDF document
+            extractor.LoadDocumentFromFile("ProductsMonthWise.pdf");
+
+            // Uncomment this line if you need all pages converted into a single worksheet:
+            //extractor.PageToWorksheet = false;
+
+            // Set the output format to XLSX
+            extractor.OutputFormat = SpreadseetOutputFormat.XLSX;
+
+            // No of Pages
+            var pageCount = extractor.GetPageCount();
+
+            for (int i = 0; i < pageCount; i++)
+            {
+                string outputName = $"page_{i + 1}.xlsx";
+
+                // Save page to spreadsheet file
+                extractor.SavePageToXLSFile(i, outputName);
+
+                Console.WriteLine("'{0}' Created", outputName);
+            }
+
+            // Cleanup
+            extractor.Dispose();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PDF Extractor SDK/VB.NET/Convert PDF To XLSX by pages/Program.vb
@@ -0,0 +1,53 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Get free API Key https://app.pdf.co/signup                     '
+'                                                                                           '
+' Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                '
+' https://www.bytescout.com                                                                 '
+' https://www.pdf.co                                                                        '
+'*******************************************************************************************'
+
+
+Imports Bytescout.PDFExtractor
+
+Class Program
+
+    Friend Shared Sub Main(args As String())
+
+        ' Create Bytescout.PDFExtractor.XLSExtractor instance
+
+        Dim extractor As New XLSExtractor()
+        extractor.RegistrationName = "demo"
+        extractor.RegistrationKey = "demo"
+
+        ' Load sample PDF document
+        extractor.LoadDocumentFromFile("ProductsMonthWise.pdf")
+
+        ' Uncomment this line if you need all pages converted into a single worksheet:
+        'extractor.PageToWorksheet = False
+
+        ' Set the output format to XLSX
+        extractor.OutputFormat = SpreadseetOutputFormat.XLSX
+
+        ' No of Pages
+        Dim pageCount As Int32 = extractor.GetPageCount()
+
+        For i As Int32 = 0 To pageCount - 1
+            Dim outputName As String = $"page_{i + 1}.xlsx"
+
+            ' Save page to spreadsheet file
+            extractor.SavePageToXLSFile(i, outputName)
+
+            Console.WriteLine("'{0}' Created", outputName)
+        Next
+
+        ' Cleanup
+        extractor.Dispose()
+
+        Console.ReadLine()
+
+    End Sub
+
+End Class
