f52270fc9ad6e36b38de47f7961659e7e059d1c3
estar73 <estarkov@gmail.com>
2020-05-25 17:46:40 +0000

    = regular update

---
+++ PDF SDK/C#/Draw Text On Rotated Page/Program.cs
@@ -0,0 +1,106 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using Bytescout.PDF;
+using System.Diagnostics;
+using System.Drawing;
+using Brush = Bytescout.PDF.Brush;
+using Font = Bytescout.PDF.Font;
+using SolidBrush = Bytescout.PDF.SolidBrush;
+using StringFormat = Bytescout.PDF.StringFormat;
+
+
+namespace DrawHeaderAndFooterOnRotatedPage
+{
+    /// <summary>
+    /// This example demonstrates how to add header and footer to rotated PDF pages.
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            // Load document
+            Document pdfDocument = new Document(@".\RotatedPages.pdf");
+            pdfDocument.RegistrationName = "demo";
+            pdfDocument.RegistrationKey = "demo";
+
+            string headerText = "Sample Header";
+            string footerText = "Sample Footer";
+
+            // Prepare StringFormat with Center text alignment
+            StringFormat stringFormat = new StringFormat { HorizontalAlign = HorizontalAlign.Center };
+            // Prepare drawing tools
+            Font font = new Font(StandardFonts.Helvetica, 9);
+            Brush brush = new SolidBrush(new ColorRGB(255, 0, 0));
+            
+            for (int i = 0; i < pdfDocument.Pages.Count; ++i)
+            {
+                Page page = pdfDocument.Pages[i];
+                
+                page.Canvas.SaveGraphicsState();
+
+                // Calculate the coordinates of the text taking into account the page rotation:
+
+                float headerY = 10;
+                float footerY;
+                float textRectWidth;
+                float textRectHeight = font.GetTextHeight() + 5;
+
+                if (page.RotationAngle == RotationAngle.Rotate0 || page.RotationAngle == RotationAngle.Rotate180)
+                {
+                    footerY = page.Height - textRectHeight - 10;
+                    textRectWidth = page.Width;
+                }
+                else
+                {
+                    footerY = page.Width - textRectHeight - 10;
+                    textRectWidth = page.Height;
+                }
+
+                // Rotate page canvas according to page rotation
+                switch (page.RotationAngle)
+                {
+                    case RotationAngle.Rotate90:
+                        page.Canvas.RotateTransform(-90);
+                        page.Canvas.TranslateTransform(-page.Height, 0);
+                        break;
+                    case RotationAngle.Rotate180:
+                        page.Canvas.RotateTransform(180);
+                        page.Canvas.TranslateTransform(-page.Width, -page.Height);
+                        break;
+                    case RotationAngle.Rotate270:
+                        page.Canvas.RotateTransform(-270);
+                        page.Canvas.TranslateTransform(0, -page.Width);
+                        break;
+                }
+                
+                // Draw header and footer
+                page.Canvas.DrawString(headerText, font, brush, new RectangleF(0, headerY, textRectWidth, textRectHeight), stringFormat);
+                page.Canvas.DrawString(footerText, font, brush, new RectangleF(0, footerY, textRectWidth, textRectHeight), stringFormat);
+
+                page.Canvas.RestoreGraphicsState();
+            }
+            
+            // Save document to file
+            pdfDocument.Save("result.pdf");
+
+            // Cleanup 
+            pdfDocument.Dispose();
+
+            // Open result PDF document in default associated application (for demo purpose)
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("result.pdf");
+            processStartInfo.UseShellExecute = true;
+            Process.Start(processStartInfo);
+        }
+    }
+}
---
+++ PDF SDK/VB.NET/Draw Text On Rotated Page/Program.vb
@@ -0,0 +1,100 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Get free API Key https://app.pdf.co/signup                     '
+'                                                                                           '
+' Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                '
+' https://www.bytescout.com                                                                 '
+' https://www.pdf.co                                                                        '
+'*******************************************************************************************'
+
+
+Imports System.Drawing
+Imports Bytescout.PDF
+Imports Font = Bytescout.PDF.Font
+Imports SolidBrush = Bytescout.PDF.SolidBrush
+Imports StringFormat = Bytescout.PDF.StringFormat
+
+''' <summary>
+''' This example demonstrates how to add header and footer to rotated PDF pages.
+''' </summary>
+Class Program
+
+    Shared Sub Main()
+
+        ' Load PDF form
+        Dim pdfDocument = New Document("RotatedPages.pdf")
+        pdfDocument.RegistrationName = "demo"
+        pdfDocument.RegistrationKey = "demo"
+
+        Dim headerText As String = "Sample Header"
+        Dim footerText As String = "Sample Footer"
+
+        ' Prepare StringFormat with Center text alignment
+        Dim stringFormat As New StringFormat() With {.HorizontalAlign = HorizontalAlign.Center}
+
+        ' Prepare drawing tools
+        Dim font As New Font(standardFont:=StandardFonts.Helvetica, emSize:=9)
+        Dim brush As New SolidBrush(New ColorRGB(255, 0, 0))
+
+        For i As Integer = 0 To pdfDocument.Pages.Count - 1
+
+            Dim page As Page = pdfDocument.Pages(i)
+
+            page.Canvas.SaveGraphicsState()
+
+            ' Calculate the coordinates of the text taking into account the page rotation:
+            Dim headerY As Single = 10
+            Dim footerY As Single
+            Dim textRectWidth As Single
+            Dim textRectHeight As Single = font.GetTextHeight() + 5
+
+            If page.RotationAngle = RotationAngle.Rotate0 Or page.RotationAngle = RotationAngle.Rotate180 Then
+                footerY = page.Height - textRectHeight - 10
+                textRectWidth = page.Width
+            Else
+                footerY = page.Width - textRectHeight - 10
+                textRectWidth = page.Height
+            End If
+
+            ' Rotate page canvas according to page rotation
+            Select Case page.RotationAngle
+
+                Case RotationAngle.Rotate90
+                    page.Canvas.RotateTransform(-90)
+                    page.Canvas.TranslateTransform(-page.Height, 0)
+                    Exit Select
+
+                Case RotationAngle.Rotate180
+                    page.Canvas.RotateTransform(180)
+                    page.Canvas.TranslateTransform(-page.Width, -page.Height)
+                    Exit Select
+
+                Case RotationAngle.Rotate270
+                    page.Canvas.RotateTransform(-270)
+                    page.Canvas.TranslateTransform(0, -page.Width)
+                    Exit Select
+
+            End Select
+
+            ' Draw header And footer
+            page.Canvas.DrawString(headerText, font, brush, New RectangleF(0, headerY, textRectWidth, textRectHeight), stringFormat)
+            page.Canvas.DrawString(footerText, font, brush, New RectangleF(0, footerY, textRectWidth, textRectHeight), stringFormat)
+
+            page.Canvas.RestoreGraphicsState()
+
+        Next
+
+        ' Save document to file
+        pdfDocument.Save("result.pdf")
+
+        ' Cleanup 
+		pdfDocument.Dispose()
+
+        ' Open document in default PDF viewer app
+        Process.Start("result.pdf")
+
+    End Sub
+
+End Class
