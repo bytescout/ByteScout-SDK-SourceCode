98e90f2bf84213fd2aaa5dc162371dc63468a952
estar73 <estarkov@gmail.com>
2020-12-04 11:10:41 +0000

    = regular update

---
+++ PDF SDK/C#/Get All PDF Form Fields/Program.cs
@@ -0,0 +1,121 @@
+//*******************************************************************************************//
+//                                                                                           //
+// Download Free Evaluation Version From: https://bytescout.com/download/web-installer       //
+//                                                                                           //
+// Also available as Web API! Get Your Free API Key: https://app.pdf.co/signup               //
+//                                                                                           //
+// Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                //
+// https://www.bytescout.com                                                                 //
+// https://pdf.co                                                                            //
+//                                                                                           //
+//*******************************************************************************************//
+
+
+using Bytescout.PDF;
+using System;
+using System.Collections.Generic;
+
+namespace GetFormFields
+{
+    /// <summary>
+    /// This example demonstrates how to all PDF form fields programmatically.
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            // Load PDF form
+            Document document = new Document(@"sample.pdf");
+            document.RegistrationName = "demo";
+            document.RegistrationKey = "demo";
+
+            var formFields = new List<FormFieldInfo>();
+
+            // Loop through all pages
+            for (int i = 0; i < document.Pages.Count; i++)
+            {
+                var page = document.Pages[i];
+
+                // Loop through all annotations
+                foreach (var annotation in page.Annotations)
+                {
+                    if(annotation is EditBox || annotation is ComboBox || annotation is CheckBox || annotation is RadioButton)
+                    {
+                        // Get form field info
+                        var fieldInfo = new FormFieldInfo
+                        {
+                            Type = annotation.Type.ToString(),
+                            FieldName = ((Field)annotation).Name,
+                            PageIndex = i,
+                            Left = ((Field)annotation).Left,
+                            Top = ((Field)annotation).Top,
+                            Width = ((Field)annotation).Width,
+                            Height = ((Field)annotation).Height
+                        };
+
+                        // Get value
+                        switch (annotation.Type)
+                        {
+                            case AnnotationType.EditBox:
+                                fieldInfo.Value = ((EditBox)annotation).Text;
+                                break;
+                            case AnnotationType.ComboBox:
+                                fieldInfo.Value = ((ComboBox)annotation).Text;
+                                break;
+                            case AnnotationType.CheckBox:
+                                fieldInfo.Value = ((CheckBox)annotation).Checked.ToString();
+                                break;
+                            case AnnotationType.RadioButton:
+                                fieldInfo.Value = ((RadioButton)annotation).Checked.ToString();
+                                break;
+                        }
+
+                        formFields.Add(fieldInfo);
+                    }
+                }
+            }
+
+            // Cleanup 
+            document.Dispose();
+
+            // Display Form data
+            DisplayData(formFields);
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Displays data to console
+        /// </summary>
+        static void DisplayData(List<FormFieldInfo> formFieldInfos)
+        {
+            if(formFieldInfos != null && formFieldInfos.Count > 0)
+            {
+                foreach (var item in formFieldInfos)
+                {
+                    Console.WriteLine($"Type: {item.Type}\nFieldName: {item.FieldName}\nValue: {item.Value}\nPage Index: {item.PageIndex}\nLeft: {item.Left}, Top: {item.Top}, Width: {item.Width}, Height: {item.Height}");
+
+                    Console.WriteLine();
+
+                }
+            }
+        }
+
+    }
+
+    class FormFieldInfo
+    {
+        public string Type { get; set; }
+        public string FieldName { get; set; }
+        public string Value { get; set; }
+
+        public int PageIndex { get; set; }
+
+        public float Left { get; set; }
+        public float Top { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+    }
+
+
+}
---
+++ PDF SDK/VB.NET/Get All PDF Form Fields/Program.vb
@@ -0,0 +1,106 @@
+'*******************************************************************************************'
+'                                                                                           '
+' Download Free Evaluation Version From:     https://bytescout.com/download/web-installer   '
+'                                                                                           '
+' Also available as Web API! Get free API Key https://app.pdf.co/signup                     '
+'                                                                                           '
+' Copyright © 2017-2020 ByteScout, Inc. All rights reserved.                                '
+' https://www.bytescout.com                                                                 '
+' https://www.pdf.co                                                                        '
+'*******************************************************************************************'
+
+
+Imports Bytescout.PDF
+
+''' <summary>
+''' This example demonstrates how to all PDF form fields programmatically.
+''' </summary>
+Class Program
+
+    Shared Sub Main()
+
+        ' Load PDF form
+        Dim document = New Document("sample.pdf")
+        document.RegistrationName = "demo"
+        document.RegistrationKey = "demo"
+
+        Dim formFields As New List(Of FormFieldInfo)
+
+        ' Loop through all pages
+        For i = 0 To document.Pages.Count - 1
+
+            Dim page = document.Pages(i)
+
+            ' Loop through all annotations
+            For Each annotation In page.Annotations
+
+                If TypeOf annotation Is EditBox Or TypeOf annotation Is ComboBox Or TypeOf annotation Is CheckBox Or TypeOf annotation Is RadioButton Then
+
+                    ' Get form field info
+                    Dim fieldInfo As New FormFieldInfo()
+                    fieldInfo.Type = annotation.Type.ToString()
+                    fieldInfo.FieldName = CType(annotation, Field).Name
+                    fieldInfo.PageIndex = i
+                    fieldInfo.Left = CType(annotation, Field).Left
+                    fieldInfo.Top = CType(annotation, Field).Top
+                    fieldInfo.Width = CType(annotation, Field).Width
+                    fieldInfo.Height = CType(annotation, Field).Height
+
+                    ' Get form field value
+                    Select Case annotation.Type
+                        Case AnnotationType.EditBox
+                            fieldInfo.Value = CType(annotation, EditBox).Text
+                        Case AnnotationType.ComboBox
+                            fieldInfo.Value = CType(annotation, ComboBox).Text
+                        Case AnnotationType.CheckBox
+                            fieldInfo.Value = CType(annotation, CheckBox).Checked.ToString()
+                        Case AnnotationType.RadioButton
+                            fieldInfo.Value = CType(annotation, RadioButton).Checked.ToString()
+                    End Select
+
+                    formFields.Add(fieldInfo)
+
+                End If
+
+            Next
+
+        Next
+
+        ' Cleanup 
+        document.Dispose()
+
+        ' Display Form Data
+        DisplayData(formFields)
+
+        Console.ReadLine()
+
+    End Sub
+
+    ''' <summary>
+    ''' Display data to console
+    ''' </summary>
+    Shared Sub DisplayData(ByVal formFieldInfos As List(Of FormFieldInfo))
+        If Not formFieldInfos Is Nothing And formFieldInfos.Count > 0 Then
+            For Each item In formFieldInfos
+                Console.WriteLine($"Type: {item.Type}{Environment.NewLine}FieldName: {item.FieldName}{Environment.NewLine}Value: {item.Value}{Environment.NewLine}Page Index: {item.PageIndex}{Environment.NewLine}Left: {item.Left}, Top: {item.Top}, Width: {item.Width}, Height: {item.Height}")
+                Console.WriteLine()
+            Next
+        End If
+    End Sub
+
+End Class
+
+Class FormFieldInfo
+
+    Public Property Type As String
+    Public Property FieldName As String
+    Public Property Value As String
+
+    Public Property PageIndex As Int32
+
+    Public Property Left As Single
+    Public Property Top As Single
+    Public Property Width As Single
+    Public Property Height As Single
+
+End Class
